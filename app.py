b531514edda16d1c0da7d7998188f34097c5104b
sheikhabbas78 <sheikhabbas78@gmail.com>
2020-08-08 15:54:13 +0530

    add run.py

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ app.config['PROPAGATE_EXCEPTIONS']=True
 app.secret_key='abbas'
 api=Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 
 jwt=JWT(app,authenticate,identity)
